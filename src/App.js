1cd5269844348f78c9188563a9ccbf9b76fc8660
pjlee337 <lepaul@amazon.com>
2022-12-14 10:11:58 -0700

    added megashop cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import hands from './hands.svg';
 import './App.css';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -10,6 +11,9 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 
 const darkTheme = createTheme({
   palette: {
@@ -41,6 +45,76 @@ function App() {
           </div>
         </Toolbar>
       </AppBar>
+      <Box sx={{ flexGrow: 1 }} className='App-body'>
+        <Card sx={{ minWidth: 275 }} className="Megashop">
+          <CardContent>
+            <img src={hands} className="Hands-logo" alt="hands" />
+            <Typography variant="h5" component="div">
+              <br />
+              Megashop
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              Dataset 1
+            </Typography>
+            <Typography variant="body2">
+              Select this option to
+              <br />
+              configure your parameters
+              <br />
+              for Megashop A
+            </Typography>
+          </CardContent>
+          <CardActions className="Centered">
+            <Button size="small">Select</Button>
+          </CardActions>
+        </Card>
+
+        <Card sx={{ minWidth: 275 }} className="Megashop">
+          <CardContent>
+            <img src={hands} className="Hands-logo" alt="hands" />
+            <Typography variant="h5" component="div">
+              <br />
+              Megashop
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              Dataset 2
+            </Typography>
+            <Typography variant="body2">
+              Select this option to
+              <br />
+              configure your parameters
+              <br />
+              for Megashop B
+            </Typography>
+          </CardContent>
+          <CardActions className="Centered">
+            <Button size="small">Select</Button>
+          </CardActions>
+        </Card>
+
+        <Card sx={{ minWidth: 275 }} className="Megashop">
+          <CardContent>
+            <img src={hands} className="Hands-logo" alt="hands" />
+            <Typography variant="h5" component="div">
+              <br />
+              Megashop
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+              Dataset 3
+            </Typography>
+            <Typography variant="body2">
+              Select this option to
+              <br />
+              configure your parameters
+              <br />
+              for Megashop C
+            </Typography>
+          </CardContent>
+          <CardActions className="Centered">
+            <Button size="small">Select</Button>
+          </CardActions>
+        </Card>
+      </Box>
     </Box>
   );
 }
